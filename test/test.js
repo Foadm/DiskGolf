028fe46a01fdf6edc7d037f8819e803cc0544858
foad.mozaffari <foad.mozaffari@mywedding.com>
2014-06-09 10:00:31 -0700

    added a second test

--- test/test.js
+++ test/test.js
@@ -11,4 +11,15 @@ describe("Index Page", function(){
             done();
         });
     });
+    it("Should fill the player name and display the players name and score", function(done){
+        browser.visit("http://localhost:8000/index.html", function(){
+            browser.
+            fill("#playerField", "Player 1").
+            pressButton("#addPlayer"), function(){
+                assert.equal(browser.text(".player_name"), "Player 1");
+            };
+            assert.ok(browser.success);
+            done();
+        });
+    });
 });
