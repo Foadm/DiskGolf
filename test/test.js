18782cb099b90338af9afabdcfd80aac9abacb4e
foad.mozaffari <foad.mozaffari@mywedding.com>
2014-06-11 10:49:06 -0700

    first acceptance test in CasperJS

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
-        }, true);
+casper.test.begin('Disk Golf App', 2, function suite(test) {
+    casper.start("http://localhost:8000/html/index.html", function() {
+        test.assertExists('#addPlayer', "Add player button is found");
     });
 
     casper.then(function() {
-        test.assertTitle("casperjs - Recherche Google", "google title is ok");
-        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
+        this.fill('#enterPlayer', {
+            'addPlayer':    'Player1'
+        }, true);
+        casper.then(function() {
+        });
+        this.click('#addPlayer');
+        casper.then(function() {
+        });
+        test.assertSelectorHasText('.player_name', 'Player1');
     });
 
     casper.run(function() {
