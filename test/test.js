7f82e6133cacd65bee24d5d5380bebbcc45848ab
Micah Geisel <micah@botandrose.com>
2014-06-11 11:09:53 -0700

    clean up acceptance test.

--- test/test.js
+++ test/test.js
@@ -1,21 +1,9 @@
-casper.test.begin('Disk Golf App', 2, function suite(test) {
-    casper.start("http://localhost:8000/html/index.html", function() {
-        test.assertExists('#addPlayer', "Add player button is found");
-    });
-
-    casper.then(function() {
-        this.fill('#enterPlayer', {
-            'addPlayer':    'Player1'
-        }, true);
-        casper.then(function() {
-        });
-        this.click('#addPlayer');
-        casper.then(function() {
-        });
-        test.assertSelectorHasText('.player_name', 'Player1');
-    });
-
+casper.test.begin('Disk Golf App', function suite(test) {
+    casper.start("http://localhost:8000/html/index.html")
     casper.run(function() {
+        this.fill('#enterPlayer', { 'addPlayer': 'Player1' });
+        this.click('#addPlayer');
+        test.assertSelectorHasText('.player_name', 'Player1');
         test.done();
     });
 });
