b29502a07ee5c5e2eeb8e3ce6bd303e58074314a
foad.mozaffari <foad.mozaffari@mywedding.com>
2014-07-06 16:33:44 -0700

    scoring view working

--- JS/refactor.js
+++ JS/refactor.js
@@ -1,60 +1,67 @@
 
-    var PlayerModel = function(name, score) {
+    var PlayerModel = function(name, score, currentHole) {
         this.name = name;
         this.score = score;
+        this.currentHole = currentHole;
 
         this.isValid = function() {
            return this.name !== "";
         }
     };
 
-    var PlayerView = function(player) {
-        this.player = player;
+    var PlayerView = function(context) {
+        this.context = context;
         var $template = $("#player-template");
         var source = $template.html();
         var template = Handlebars.compile(source);
-        var html    = template(player);
+        var html    = template(context);
         $template.after(html);
         //$('.player_name').append('<span class="error">You must enter a valid name</span>').append("<br>");
     };
 
     var PlayerController = function(){
+        //this.playerArray = [];
         this.addPlayer = function(name){
             var score = 0;
-            this.player = new PlayerModel(name, score);
+            var currentHole = 0;
+            this.player = new PlayerModel(name, score, currentHole);
             this.view = new PlayerView(this.player);
+            //this.playerArray.push(name);
+            $('#start-button').click({param : this.player}, initializeScoring);
+            function initializeScoring(e, param){
+                this.scoring = new ScoringController(e.data.param);
+                this.scoring.updateView();
+            }
         };
     };
 
-    var ScoringController = function(){
+    var ScoringController = function(player){
+        this.player = player;
         this.updateView = function(){
-            this.scoringView = new ScoringView();
+            this.scoringView = new ScoringView(this.player);
             console.log("scoring view is instantiated");
         };
     };
 
-    var ScoringView = function(player, state){
+    var ScoringView = function(context, currentHole){
         console.log("scoring view is called");
-        this.player = player;
-        this.state = state;
+        this.context = context;
+        //this.score = score;
         var $template = $("#scoring-template");
         var source = $template.html();
         var template = Handlebars.compile(source);
-        var html    = template(player, state);
+        var html    = template(context);
         $template.after(html);
     };
 
     // add players to the game
     $( document ).ready(function() {
         var playerController = new PlayerController();
-        var scoringController = new ScoringController();
+        //var scoringController = new ScoringController();
 
         $('#addPlayer').click(function() {
             var playerName = $('#playerField').val();
             playerController.addPlayer(playerName);
-        });
-        $('#start-button').click(function() {
-            scoringController.updateView();
         });
     });
 
